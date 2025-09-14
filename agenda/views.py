fb34d86a39bdb4a2ab4ef310b317fa94977ec572
Joab Vieira de França <joabvieiradefranca@gmail.com>
2025-09-14 20:33:33 -0300

    commit na primeira tela

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.utils.timezone import now
+from .models import Agendamento
+
+def dashboard(request):
+    hoje = now().date()
+    agendamentos_hoje = Agendamento.objects.filter(data=hoje)
+
+    context = {
+        "total_agendamentos_hoje": agendamentos_hoje.count(),
+        "agendamentos": Agendamento.objects.all().order_by("data", "hora")
+    }
+    return render(request, "agenda/dashboard.html", context)
--- agendamento/settings.py
+++ agendamento/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     'agenda',
 ]
 
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
